69b0e82fb6697be40e0e148bf9453c9ca60f0245
ckkashi <ckkashi007@gmail.com>
2021-05-23 13:57:48 -0700

    Second Assignment complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,76 @@
- import 'package:flutter/material.dart';
+import 'dart:ui';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My First App",
+      debugShowCheckedModeBanner: false,
+      title: "My Second Assignment",
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
       home: Scaffold(
-        backgroundColor: Colors.blueAccent,
         appBar: AppBar(
-          title:Text(
-            "My App"
-            ,style: TextStyle(
-              fontFamily: "comic sans MS"
-            ),
-            )
-        ),
-        body: Center(
-          child:Text(
-            "Kashif Shaikh\nckkashi007@gmail.com",
-            style:TextStyle(
-              fontSize: 20.0,
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1.5,
-              height: 2.5,
-              fontFamily: "comic sans MS",
-              background: Paint()..color=Colors.white
-            )
+          title:Center(
+            child: Text("Second Assignment of Flutter"),
           )
+        ),
+        body: SingleChildScrollView(
+                  child: Center(
+
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height:20),
+                Text("Login",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(height:20),
+                Container(
+                  child: TextField(decoration: InputDecoration(
+
+                    border: OutlineInputBorder(),
+                    hintText: "Enter E-mail",
+
+                  ),),
+                  width: 300,
+                  height: 60,
+                ),//login textfield container
+                SizedBox(height:5),
+                Container(
+                  child: TextField(
+                    decoration: InputDecoration(
+                    
+                    border: OutlineInputBorder(),
+                    hintText: "Enter Password",
+
+                    ),
+                    obscureText: true,
+                    
+                  ),//password textfield container
+                  width: 300,
+                  height: 60,
+                ),
+                ElevatedButton(
+                  onPressed: (){},
+                 child: Text("LOGIN"),
+                 ),
+
+              ],
+
+            ),
+
+          ),
         ),
       ),
 
